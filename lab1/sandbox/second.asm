49f334df6a572cbea33d48c50260299b5499862e
Klim <131042512+kluv1n@users.noreply.github.com>
2024-09-14 00:36:18 +0000

    доделал 1 лабу

---
+++ lab1/sandbox/second.asm
@@ -0,0 +1,15 @@
+format ELF
+public _start
+msg db "Kryzhniy", 0xA, 0, "Klim", 0xA, 0, "Sergeevich", 0xA, 0
+
+_start:
+    ;инициализация регистров для вывода информации на экран
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, 25
+    int 0x80
+    ;инициализация регистров для успешного завершения работы программы
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
