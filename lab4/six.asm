3f93d159c63bb06fa619e154ee896e41791d531b
Klim <131042512+kluv1n@users.noreply.github.com>
2024-11-16 11:03:09 +0000

    сохранение

--- lab4/six.asm
+++ lab4/six.asm
@@ -1,78 +1,70 @@
-format ELF64                  ; Указываем 64-битный ELF формат
-entry _start                  ; Точка входа в программу
+format ELF64
 
-section .bss                  ; Секция для неинициализированных данных
-    input_buffer resb 256     ; Буфер для ввода (256 байт)
-    result_buffer resb 32     ; Буфер для результата (32 байта)
-    count resb 1              ; Переменная для хранения количества чисел
+public _start
 
-section .text                 ; Секция кода, должна быть исполняемой
-    ; Внешние функции
-    extern input_keyboard
-    extern str_number
-    extern number_str
-    extern print_str
-    extern new_line
-    extern exit
+include 'func.asm'
 
-_start:
-    ; Чтение числа пользователя
+section '.bss' writable
+  output dq 0
+  input_buffer rb 255
+
+section '.text' executable
+  _start:
+    mov rax, 0
+    mov rdi, 0
     mov rsi, input_buffer
-    call input_keyboard       ; Ввод числа с клавиатуры
-
-    ; Преобразование строки в число
+    call input_keyboard
     mov rsi, input_buffer
-    call str_number           ; Преобразование строки в число, результат в RAX
+    call str_number
+    xor r8, r8
 
-    ; Сброс счетчика
-    xor rbx, rbx              ; count = 0
-    mov rcx, 1                ; Начинаем проверку с числа 1
+    mov rbx, rax
+    xor rdi, rdi
+    cmp rax, 5
+    jl .finish
+    mov rsi, 5
 
-check_loop:
-    cmp rcx, rax              ; Сравниваем текущее значение с введенным числом
-    ja end_loop               ; Если больше, выходим из цикла
+  .main_loop:
+     xor rcx, rcx
+     xor rdx, rdx
+     cmp rsi, rbx
+     jg .finish
 
-    ; Проверка делимости на 5
-    mov rdx, 0
-    mov rbx, rcx
-    mov rdi, 5
-    div rdi                   ; rax = rcx / 5, rdx = rcx % 5
-    test rdx, rdx             ; Проверяем остаток от деления на 5
-    jnz next_number           ; Если не делится на 5, переходим к следующему числу
+  .check_seven_div:
+     xor rax, rax
+     mov rax, rsi
+     xor rcx, rcx
+     xor rdx, rdx
+     mov rcx, 7
+     div rcx
+     cmp rdx, 0
+     jg .check_three_div
 
-    ; Проверка делимости на 3
-    mov rdx, 0
-    mov rbx, rcx
-    mov rdi, 3
-    div rdi                   ; rax = rcx / 3, rdx = rcx % 3
-    test rdx, rdx             ; Проверяем остаток от деления на 3
-    jz next_number            ; Если делится на 3, переходим к следующему числу
+     add rsi, 5
+     cmp rsi, rbx
+     jg .finish
 
-    ; Проверка делимости на 7
-    mov rdx, 0
-    mov rbx, rcx
-    mov rdi, 7
-    div rdi                   ; rax = rcx / 7, rdx = rcx % 7
-    test rdx, rdx             ; Проверяем остаток от деления на 7
-    jz next_number            ; Если делится на 7, переходим к следующему числу
+  .check_three_div:
+     xor rax, rax
+     mov rax, rsi
+     xor rcx, rcx
+     xor rdx, rdx
+     mov rcx, 3
+     div rcx
+     cmp rdx, 0
+     jg .increment_counter
 
-    ; Если число прошло все проверки, увеличиваем счетчик
-    inc byte [count]
+     add rsi, 5
+     cmp rsi, rbx
+     jg .finish
 
-next_number:
-    inc rcx                   ; Увеличиваем текущее число
-    jmp check_loop            ; Возвращаемся в начало цикла
+  .increment_counter:
+     add rdi, 1
+     add rsi, 5
+     cmp rsi, rbx
+     jl .main_loop
 
-end_loop:
-    ; Преобразование результата в строку
-    movzx rax, byte [count]
-    mov rsi, result_buffer
-    call number_str
-
-    ; Вывод результата
-    mov rsi, result_buffer
-    call print_str
-
-    ; Переход на новую строку и завершение программы
-    call new_line
+.finish:
+    mov rax, rdi
+    call print_num
     call exit
